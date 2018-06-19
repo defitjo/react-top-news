5b3d52fece102e21e0f6063a13f3bf8e7655160d
Devon <defitjo16@gmail.com>
2018-06-19 00:14:43 -0400

    README and styling

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Top News of the Day</h1>
+          <h1 className="App-title">React Top News</h1>
         </header>
         <div className="Top-news">
           <NewsItem />
